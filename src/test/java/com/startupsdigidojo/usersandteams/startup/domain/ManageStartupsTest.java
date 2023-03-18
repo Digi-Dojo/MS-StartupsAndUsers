ed8e50ca4ec48ab00a09b4dfd8c5c2f1672dac4f
Matteo Larcher <matteo.larcher.01@gmail.com>
2023-03-18 16:41:57 +0100

    Added tests for ManageStartups and SearchStartups methods

--- src/test/java/com/startupsdigidojo/usersandteams/startup/domain/ManageStartupsTest.java
+++ src/test/java/com/startupsdigidojo/usersandteams/startup/domain/ManageStartupsTest.java
@@ -41,6 +41,81 @@ public class ManageStartupsTest {
                 .isGreaterThan(0);
     }
 
+    @Test
+    public void createStartupThrowsForExistingStartup(){
+        Startup startup = new Startup("GreenGym", "An eco-friendly environment to train in");
+
+        when(startupRepository.findByName(anyString()))
+                .thenReturn(Optional.of(new Startup("GreenGym", "An eco-friendly environment to train in")));
+
+        assertThatThrownBy(() -> underTest.createStartup(startup.getName(), startup.getDescription()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void updateStartupNameUpdatesTheName(){
+        String oldName = "GreenGym";
+        String newName = "PlanetFitness";
+        String description = "Description";
+        when(startupRepository.findByName(newName)).thenReturn(Optional.empty());
+        when(startupRepository.findByName(oldName)).thenReturn(Optional.of(new Startup(randomPositiveLong(),oldName,description)));
+        when(startupRepository.save(any())).thenReturn(new Startup(randomPositiveLong(),newName,description));
+
+        Startup result = underTest.updateStartupName(oldName,newName);
+
+        assertThat(result.getName()).isEqualTo(newName);
+    }
+
+    @Test
+    public void updateStartupDescriptionUpdatesTheDescription(){
+        String name = "GreenGym";
+        String description = "Description";
+        when(startupRepository.findByName(name)).thenReturn(Optional.of(new Startup(randomPositiveLong(),name,"old description")));
+        when(startupRepository.save(any())).thenReturn(new Startup(randomPositiveLong(),name,description));
+
+        Startup result = underTest.updateStartupDescription(name,description);
+
+        assertThat(result.getDescription()).isEqualTo(description);
+    }
+
+    @Test
+    public void updateStartupNameThrowsForNewNameExisting(){
+        String newName = "newName";
+        String oldName = "oldName";
+        when(startupRepository.findByName(newName)).thenReturn(Optional.of(new Startup(newName,"description")));
+        assertThatThrownBy(() -> underTest.updateStartupName(oldName,newName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void updateStartupNameThrowsForOldNameNotExisting(){
+        String newName = "newName";
+        String oldName = "oldName";
+        when(startupRepository.findByName(anyString())).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> underTest.updateStartupName(oldName,newName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void updateStartupDescriptionThrowsForNameNotExisting(){
+        String name = "name";
+        String description = "description";
+        when(startupRepository.findByName(anyString())).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> underTest.updateStartupDescription(name,description))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void deleteStartupThrowsForNotExistingStartup(){
+        String name = "name";
+        when(startupRepository.findByName(anyString()))
+                .thenReturn(Optional.empty());
+        assertThatThrownBy(() -> underTest.deleteStartup(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
     private Long randomPositiveLong() {
         long leftLimit = 1L;
         long rightLimit = 1000L;
