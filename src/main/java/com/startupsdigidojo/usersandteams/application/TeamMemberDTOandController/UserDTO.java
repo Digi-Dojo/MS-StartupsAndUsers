2fcded5584b3d2f0b81531e8549221e3221c9172
Tobias Unterhauser <tobias.unterhauser@stud-inf.unibz.it>
2023-03-19 20:59:50 +0100

    added User dto and and controller

---
+++ src/main/java/com/startupsdigidojo/usersandteams/application/TeamMemberDTOandController/UserDTO.java
@@ -0,0 +1,41 @@
+package com.startupsdigidojo.usersandteams.application.TeamMemberDTOandController;
+
+public class UserDTO {
+
+    String name;
+
+    String password;
+
+    String mailAddres;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMailAddres() {
+        return mailAddres;
+    }
+
+    public void setMailAddres(String mailAddres) {
+        this.mailAddres = mailAddres;
+    }
+
+
+    public UserDTO(String name, String password, String mailAddres) {
+        this.name = name;
+        this.password = password;
+        this.mailAddres = mailAddres;
+    }
+}
