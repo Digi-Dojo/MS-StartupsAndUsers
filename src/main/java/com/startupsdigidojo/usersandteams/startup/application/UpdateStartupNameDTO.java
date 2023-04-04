789d64d119343f97d6c6f76a5810d385da463264
Gianluca Feller <gifeller@unibz.it>
2023-04-04 15:47:51 +0200

    Added the DTOs for Startup and updated the controller

---
+++ src/main/java/com/startupsdigidojo/usersandteams/startup/application/UpdateStartupNameDTO.java
@@ -0,0 +1,28 @@
+package com.startupsdigidojo.usersandteams.startup.application;
+
+public class UpdateStartupNameDTO {
+
+    private String oldName;
+    private String newName;
+
+    public UpdateStartupNameDTO(String oldName, String newName) {
+        this.oldName = oldName;
+        this.newName = newName;
+    }
+
+    public String getOldName() {
+        return oldName;
+    }
+
+    public void setOldName(String oldName) {
+        this.oldName = oldName;
+    }
+
+    public String getNewName() {
+        return newName;
+    }
+
+    public void setNewName(String newName) {
+        this.newName = newName;
+    }
+}
