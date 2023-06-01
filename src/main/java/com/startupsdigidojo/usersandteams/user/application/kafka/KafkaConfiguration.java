9a5079ba84a35383c2c92c6a6d82a98ea4c1d273
Matteo Larcher <matteo.larcher.01@gmail.com>
2023-06-01 17:04:02 +0200

    Implemented kafka in cloud

--- src/main/java/com/startupsdigidojo/usersandteams/user/application/kafka/KafkaConfiguration.java
+++ src/main/java/com/startupsdigidojo/usersandteams/user/application/kafka/KafkaConfiguration.java
@@ -16,10 +16,22 @@ public class KafkaConfiguration {
     @Value("${spring.kafka.properties.bootstrap.servers}")
     private String bootstrapServers;
 
+    @Value("${spring.kafka.properties.sasl.mechanism}")
+    private String saslMechanism;
+
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    private String saslJaasConfig;
+
+    @Value("${spring.kafka.properties.security.protocol}")
+    private String securityProtocol;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         return new DefaultKafkaProducerFactory<>(Map.of(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+                "sasl.mechanism", saslMechanism,
+                "sasl.jaas.config", saslJaasConfig,
+                "security.protocol", securityProtocol,
                 ProducerConfig.RETRIES_CONFIG, 0,
                 ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432,
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
